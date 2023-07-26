22cd12e5925c3179aec0c77320314035dd164dc7
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-07-26 18:12:47 +0100

    Add files via upload

--- demo-script.js
+++ demo-script.js
@@ -6,25 +6,3 @@ function changeFont(font) {
 
     document.getElementById(`demo-font-${font}`).classList.add("segmented-button-active");
 }
-
-function chip(action) {
-    if (action == "chip") {
-        if (!document.getElementById("demo-chip").innerHTML.includes("span")) {
-            document.getElementById("demo-chip").innerHTML = "<span class='material-symbols-rounded'>done</span>Chip";
-        }
-
-        else {
-            document.getElementById("demo-chip").innerHTML = "Chip";
-        }
-    }
-
-    else {
-        if (!document.getElementById("demo-chip-input").innerHTML.includes("span")) {
-            document.getElementById("demo-chip-input").innerHTML += "<span class='material-symbols-rounded'>close</span>"
-        }
-
-        else {
-            document.getElementById("demo-chip-input").innerHTML = "Input";
-        }
-    }
-}
