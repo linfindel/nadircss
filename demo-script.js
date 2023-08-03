f38309d7fe03f0bb2b0b68ff7bb9bf22b7670f8f
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-08-03 16:52:09 +0100

    Delete demo-script.js

--- demo-script.js
+++
@@ -1,8 +0,0 @@
-function changeFont(font) {
-    document.body.className = `body-font-${font}`;
-
-    var buttons = document.querySelectorAll('.segmented-button button');
-    buttons.forEach(button => button.classList.remove('segmented-button-active'));
-
-    document.getElementById(`demo-font-${font}`).classList.add("segmented-button-active");
-}
