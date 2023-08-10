982dab47c73959772e104f3c6d24d593f4f48808
Nadir Technologies <116459177+Nadir-Software@users.noreply.github.com>
2023-08-10 20:19:33 +0100

    Add files via upload

--- mobile.js
+++ mobile.js
@@ -1,6 +1,8 @@
 if (/Android|iPhone/i.test(navigator.userAgent)) {
     // User is on a mobile device
     console.log('User is on a mobile device.');
+
+    open("https://nadir-software.github.io/nadircss/mobile.html", "_blank");
 }
 
 else {
